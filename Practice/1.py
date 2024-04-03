d7c5a1f09043071ef09ab61421b75be3dad91363
Lenhan231 <nhanle221199@gmail.com>
2024-04-03 08:58:38 +0700

    Optimize to nlogn

---
+++ Practice/1.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+        hashtable = dict()
+        for i in range(len(nums)):
+            if nums[i] not in hashtable:
+                hashtable[nums[i]] = i
+        print(hashtable)
+        for i in range(len(nums)):
+        
+            if (target - nums[i]) in hashtable and hashtable[target - nums[i]] != i:
+                return i, hashtable[target - nums[i]]
+nums = [3,2,4]
+target = 6
+print(twoSum(nums, target))
